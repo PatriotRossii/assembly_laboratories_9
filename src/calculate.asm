d218bb26102a127b079dc98ae65d83e9c0276bee
PatriotRossii <patriotrossii2019@mail.ru>
2023-05-20 18:37:55 +0300

    Replace SSE instructions with AVX instructions

--- src/calculate.asm
+++ src/calculate.asm
@@ -4,22 +4,22 @@ Calculate_:
         ; 1st - rdi, 2nd - rsi
         ; 3rd - rdx, 4th - rcx, 5th - r8, 6th - r9
 
-        movsd xmm0, [rdi]
-        movsd xmm1, [rsi]
+        vmovsd xmm0, [rdi]
+        vmovsd xmm1, [rsi]
 
         movapd xmm2, xmm0
-        addsd xmm2, xmm1
-        movsd [rdx], xmm2
+        vaddsd xmm2, xmm1
+        vmovsd [rdx], xmm2
 
         movapd xmm2, xmm0
-        subsd xmm2, xmm1
-        movsd [rcx], xmm2
+        vsubsd xmm2, xmm1
+        vmovsd [rcx], xmm2
 
         movapd xmm2, xmm0
-        mulsd xmm2, xmm1
-        movsd [r8], xmm2
+        vmulsd xmm2, xmm1
+        vmovsd [r8], xmm2
 
-        divsd xmm0, xmm1
-        movsd [r9], xmm0
+        vdivsd xmm0, xmm1
+        vmovsd [r9], xmm0
 
         ret
